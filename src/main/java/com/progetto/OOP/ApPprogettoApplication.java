ef1a14077abb8c73670a514b1c3bfa2d79e152ac
Riccardo Iobbi <s1093212@studenti.univpm.it>
2020-12-21 15:35:25 +0100

    inizio progetto

---
+++ src/main/java/com/progetto/OOP/ApPprogettoApplication.java
@@ -0,0 +1,13 @@
+package com.progetto.OOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApPprogettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApPprogettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progetto/OOP/ApPprogettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progetto.OOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApPprogettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
