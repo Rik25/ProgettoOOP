78d643a65831f761c7a7dd4f2a20f95ac874a240
Riccardo Iobbi <s1093212@studenti.univpm.it>
2020-12-26 10:48:19 +0100

    creata class metadata

---
+++ src/main/java/com/progetto/OOP/model/Metadata.java
@@ -0,0 +1,53 @@
+package com.progetto.OOP.model;
+/**
+ * Rappresenta la classe i cui oggetti cotengono alias assegnato,
+ * nome della colonna e tipo di dato.
+ * @author rikii
+ *
+ */
+public class Metadata {
+	
+	private String alias;
+	private String sourcefield;
+	private String type;
+	
+	public Metadata() {}
+	
+	
+	public Metadata(String alias, String sourcefield, String type) {
+		this.alias = alias;
+		this.sourcefield = sourcefield;
+		this.type = type;
+	}
+
+	
+	public String getAlias() {
+		return alias;
+	}
+
+	
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+	
+	public String getSourcefield() {
+		return sourcefield;
+	}
+
+	
+	public void setSourcefield(String sourcefield) {
+		this.sourcefield = sourcefield;
+	}
+
+	
+	public String getType() {
+		return type;
+	}
+
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
