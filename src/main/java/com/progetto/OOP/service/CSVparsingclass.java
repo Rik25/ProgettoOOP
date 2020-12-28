015dd3a28de5ef3cfcf63fe7292db749cfea094c
Riccardo Iobbi <s1093212@studenti.univpm.it>
2020-12-28 17:12:16 +0100

    Aggiunto medoto CSV, manca gestione errori

--- src/main/java/com/progetto/OOP/service/CSVparsingclass.java
+++ src/main/java/com/progetto/OOP/service/CSVparsingclass.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.progetto.OOP.model.Record;
 
@@ -17,9 +18,32 @@ import com.progetto.OOP.model.Record;
  */
 
 public class CSVparsingclass {
-	String CSVfile = "";
 	
-	public static ArrayList<Record> RunParsing(){
+	/**
+	 * Effettua il parsing del file CSV in modo da salvare ogni riga come un oggetto Record.
+	 * @param csvFile percorso del file da cui recuperare le informazioni.
+	 * @return ArrayList di Record.
+	*/
+	public static ArrayList<Record> RunParsing(String csvFile){
+		
+		int riga = 1;
+		String linea = "";
+		ArrayList<Record> records = new ArrayList<>();
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(csvFile));
+		bufferedReader.readLine(); //salto la prima riga in quanto rappresenta il nome delle colonne
+		
+		while((linea = bufferedReader.readLine()) != null) {
+			String[] recuperato = linea.split(";");//Divido quando trovo ";"
+			//Creo l'oggetto composto dai campi ottenuti dal file
+			Record oggettoRecuperato = new Record(recuperato[0].replaceAll("^\\s+",""), recuperato[1].replaceAll("^\\s+",""), 
+												recuperato[2].replaceAll("^\\s+",""), Double.parseDouble(recuperato[3]), 
+												Double.parseDouble(recuperato[4]), Double.parseDouble(recuperato[5]), 
+												Double.parseDouble(recuperato[6]), recuperato[7].replaceAll("^\\s+",""));
+			records.add(oggettoRecuperato);
+			riga++;
+		}
+		bufferedReader.close();
+		return records;
 		
 	}
 	
