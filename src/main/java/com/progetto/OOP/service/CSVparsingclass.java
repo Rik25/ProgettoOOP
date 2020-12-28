1d0fa2c7fe8e59267d1a1cfb3bab0ed6ba1af4a9
Riccardo Iobbi <s1093212@studenti.univpm.it>
2020-12-28 16:40:16 +0100

    Creazione classe CSV

---
+++ src/main/java/com/progetto/OOP/service/CSVparsingclass.java
@@ -0,0 +1,31 @@
+package com.progetto.OOP.service;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.progetto.OOP.model.Record;
+
+/**
+ * Rappresenta la classe che si occupa di popolare l'ArrayList di record
+ * prendendoli in input da un file CSV.
+ * @author Riccardo Iobbi
+ * @author Lorenzo Benenchia
+ *
+ */
+
+public class CSVparsingclass {
+	String CSVfile = "";
+	
+	public static ArrayList<Record> RunParsing(){
+		
+	}
+	
+
+}
+
+	
+
+	
