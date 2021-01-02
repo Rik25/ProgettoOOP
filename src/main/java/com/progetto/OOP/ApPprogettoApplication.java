403a24fdbe9ff57e14bb6be666708c7a0c7e6d60
Riccardo Iobbi <s1093212@studenti.univpm.it>
2021-01-02 13:33:22 +0100

    Aggiunto caricamento dal csv ad avvio programma

--- src/main/java/com/progetto/OOP/ApPprogettoApplication.java
+++ src/main/java/com/progetto/OOP/ApPprogettoApplication.java
@@ -3,10 +3,13 @@ package com.progetto.OOP;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.progetto.OOP.archivio.ArchivioClass;
+
 @SpringBootApplication
 public class ApPprogettoApplication {
 
 	public static void main(String[] args) {
+		ArchivioClass.setRecordCSV();
 		SpringApplication.run(ApPprogettoApplication.class, args);
 	}
 
