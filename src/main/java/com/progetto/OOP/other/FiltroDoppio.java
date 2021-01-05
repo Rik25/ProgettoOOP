b500582ead2d051860a448c28d1a47d4487a3078
Riccardo Iobbi <s1093212@studenti.univpm.it>
2021-01-05 11:16:27 +0100

    Create classi per gestire filtri

---
+++ src/main/java/com/progetto/OOP/other/FiltroDoppio.java
@@ -0,0 +1,100 @@
+package com.progetto.OOP.other;
+
+import java.util.ArrayList;
+/**
+ * Superclasse per i filtri che utilizzano due double
+ * @author Riccardo Iobbi
+ *@author Lorenzo Benenchia
+ */
+public class FiltroDoppio {
+	
+	protected double parametro1;
+	protected double parametro2;
+	
+	/** Costruttore 
+	 * @param parametri deve essere un ArrayList di Double di lunghezza 2.
+	 * @throws IllegalArgumentException parametri non è del tipo richiesto. 
+	*/
+	
+	public FiltroDoppio(Object parametri){
+
+		if(parametri instanceof ArrayList<?>) {
+				
+			if( ((ArrayList<?>) parametri).size() != 2) {
+				throw new IndexOutOfBoundsException("2 Double ") ;
+			}
+												
+			if(((ArrayList<?>) parametri).get(0) instanceof Double) {
+				parametro1 = (double) ((ArrayList<?>) parametri).get(0) ;
+			}
+			else {
+				throw new IllegalArgumentException("Type: Double ");}
+					
+			if(((ArrayList<?>) parametri).get(1) instanceof Double) {
+				parametro2 = (double) ((ArrayList<?>) parametri).get(1) ;
+			}
+			else{
+				throw new IllegalArgumentException("Type: Double ");}			
+					
+					
+		}else {
+			throw new IllegalArgumentException("Type: Array of 2 Double ");
+		}
+		
+		if(parametro2 < parametro1) {
+			throw new IllegalArgumentException("Second Double must be greater than the first Double ");
+		}
+	}
+	
+	/** Set
+	 * @param parametro deve essere un ArrayList di interi di lunghezza 2.
+	 * @throws IllegalArgumentException parametri non è del tipo richiesto. 
+	*/
+	
+	public void setParametri (Object parametri) {
+		
+		if(parametri instanceof ArrayList<?>) {
+			
+			if( ((ArrayList<?>) parametri).size() != 2) {
+				throw new IndexOutOfBoundsException("2 Double ") ;
+			} 
+												
+			if(((ArrayList<?>) parametri).get(0) instanceof Double) {
+				parametro1 = (double) ((ArrayList<?>) parametri).get(0) ;
+			} 
+			else {
+				throw new IllegalArgumentException("Type: Double ");}
+					
+			if(((ArrayList<?>) parametri).get(1) instanceof Integer) {
+				parametro2 = (double) ((ArrayList<?>) parametri).get(1) ;
+			} 
+			else{
+				throw new IllegalArgumentException("Type: Double ");}			
+					
+			
+		}else {
+			throw new IllegalArgumentException("Type: Array of 2 Double ");
+		}
+		
+		if(parametro2 < parametro1) {
+			throw new IllegalArgumentException("Second Double must be greater than the first Double ");
+		}
+	}
+	
+	public void setParametro1(double parametro1) {
+		this.parametro1 = parametro1;
+	}
+	
+	public void setParametro2(double parametro2) {
+		this.parametro2 = parametro2;
+	}
+	
+	public double getParametro1() {
+		return parametro1;
+	}
+	
+	public double getParametro2() {
+		return parametro2;
+	}
+
+}
---
+++ src/main/java/com/progetto/OOP/other/FiltroSingolo.java
@@ -0,0 +1,52 @@
+package com.progetto.OOP.other;
+/**
+ * Superclasse per filtri che lavorano su un double.
+ * @author Riccardo Iobbi
+ * @author Lorenzo Benenchia
+ *
+ */
+public class FiltroSingolo {
+	
+	protected double parametro;
+	/**
+	 * Costruttore della classe.
+	 * @param parametro che deve essere di tipo Double.
+	 * @throws IllegalArgumentException se il parametro non è del tipo richiesto.
+	 */
+	
+	public FiltroSingolo(Object parametro)
+	{
+		if(parametro instanceof Double)
+		{
+			this.parametro = (Double) parametro;
+		}
+		else {
+			throw new IllegalArgumentException("Type: Double ");
+		}
+	}
+	
+	/** Set
+	 * @param parametro che deve essere di tipo Double.
+	 * @throws IllegalArgumentException se il parametro non è del tipo richiesto. 
+	*/
+	public void setParametro(Object parametro) {
+		
+		if(parametro instanceof Double ) { 
+			this.parametro =(Double)parametro;
+		}else {
+			throw new IllegalArgumentException("Type: Interger ");
+		}
+	}
+	/**
+	 * set Overload
+	 * @param parametro
+	 */
+	public void setParametro(int parametro) {
+		this.parametro = parametro;
+	}
+	
+	public double getParametro() {
+		return parametro;
+	}
+
+}
