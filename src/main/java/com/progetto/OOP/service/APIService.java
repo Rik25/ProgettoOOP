327f9c57583f40fff13538ede2f06b0481f6bd60
Riccardo Iobbi <s1093212@studenti.univpm.it>
2021-01-19 10:32:23 +0100

    gestite eccezioni

--- src/main/java/com/progetto/OOP/service/APIService.java
+++ src/main/java/com/progetto/OOP/service/APIService.java
@@ -2,9 +2,13 @@ package com.progetto.OOP.service;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 import com.progetto.OOP.model.Record;
@@ -15,23 +19,54 @@ public class APIService {
 	public static Record request(String citta, String lingua, String unita) {
 		//recupero l'API key da file
 		File file = new File("FileConfig/key.txt");
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String key = br.readLine();
-		br.close();
-		
-		URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q="+citta+"&lang="+lingua+"&units="+unita+"&appid="+key);
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setRequestMethod("GET");
-		int status = con.getResponseCode();
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-				String inputLine;
-				String content = "";
-				while ((inputLine = in.readLine()) != null) {
-				    content = inputLine;
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String key = "";
+		try {
+			key = br.readLine();
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+		URL url = null;
+		try {
+			url = new URL("http://api.openweathermap.org/data/2.5/weather?q="+citta+"&lang="+lingua+"&units="+unita+"&appid="+key);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		HttpURLConnection con = null;
+		try {
+			con = (HttpURLConnection) url.openConnection();
+				
+			try {
+				con.setRequestMethod("GET");
+				} catch (ProtocolException e) {
+						e.printStackTrace();
 				}
-				in.close();
-				con.disconnect();
-				return Jsonparsingclass.jsonApiParse(content, citta, lingua, unita);
+			int status = con.getResponseCode();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			String inputLine;
+			String content = "";
+			try {
+				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+						
+						
+				while ((inputLine = in.readLine()) != null) {
+					    content = inputLine;
+					}
+					in.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+			con.disconnect();
+			return Jsonparsingclass.jsonApiParse(content, citta, lingua, unita);
 	}
 	
 }
