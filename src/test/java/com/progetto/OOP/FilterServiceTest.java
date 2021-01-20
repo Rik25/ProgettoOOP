ca56e570fdf9c31cc84489ce3894d8fa3d1cf187
Riccardo Iobbi <s1093212@studenti.univpm.it>
2021-01-20 17:29:20 +0100

    create classi test

---
+++ src/test/java/com/progetto/OOP/FilterServiceTest.java
@@ -0,0 +1,38 @@
+package com.progetto.OOP;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.progetto.OOP.eccezioni.FilterIllegalArgumentException;
+import com.progetto.OOP.eccezioni.FiltroNonTrovato;
+import com.progetto.OOP.service.FilterService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FilterServiceTest {
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	/**
+	 * test che controlla che venga lanciata l'eccezione FiltroNonTrovato
+	 */
+	@SuppressWarnings("restriction")
+	@Test
+	void test1()
+	{  
+		assertThrows(FiltroNonTrovato.class, ()->FilterService.istanziaFiltro("Provincia", "Greater", 4.0));
+	}
+	/**
+	 * test che controlla che venga lanciata l'eccezione FilterIllegalArgumentException
+	 */
+	@SuppressWarnings("restriction")
+	@Test
+	void test2() {
+		assertThrows(FilterIllegalArgumentException.class, ()->FilterService.istanziaFiltro("Citta", "In", 4.0));
+	}
+}
---
+++ src/test/java/com/progetto/OOP/StatisticheServiceTest.java
@@ -0,0 +1,32 @@
+package com.progetto.OOP;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.progetto.OOP.model.Record;
+import com.progetto.OOP.service.StatisticheService;
+import com.progetto.OOP.archivio.ArchivioClass;
+import com.progetto.OOP.eccezioni.StatisticaNonTrovata;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticheServiceTest {
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	/**
+	 * test che controlla che venga lanciata l'eccezione StatisticaNonTrovata
+	 */
+	@SuppressWarnings("restriction")
+	@Test
+	void test() {
+		ArrayList<Record> records = ArchivioClass.getRecords();
+		assertThrows(StatisticaNonTrovata.class, ()->StatisticheService.instanceStatisticheCalcolatore("Double", records) );
+	}
+}
